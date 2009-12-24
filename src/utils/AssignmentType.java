c31783f9e2c47c6d35e04f0dcc5679f07185adc8
nonother <nonother@414fe6b6-edb3-11de-91ba-91c70a792394>
2009-12-24 22:05:19 +0000

    Fully commented utils.Assignment, utils.AssignmentType, utils.Points. Gave Assignment a package-private constructor to ensure no one attempts to explicitly create one as it should only be constructed by  the ConfigurationManager.

--- src/utils/AssignmentType.java
+++ src/utils/AssignmentType.java
@@ -1,21 +1,35 @@
 package utils;
 
-public enum AssignmentType
-{
-	FINAL, PROJECT, LAB, HOMEWORK;
-	
-	public static AssignmentType getInstance(String typeString)
-	{		
-		for(AssignmentType type : values())
-		{
-			if(typeString.toUpperCase().equals(type.toString()))
-			{
-				return type;
-			}
-		}
-		
-		throw new RuntimeException("Invalid input: " + typeString +
-                                   ", valid options are " +
-                                   java.util.Arrays.toString(values()));
-	}
+/**
+ * Supports the various types of assignments a course may have:
+ *      PROJECT, LAB, HOMEWORK, and FINAL
+ *
+ * FINAL is treated as a special kind of PROJECT.
+ *
+ * @author jak2 (Joshua Kaplan)
+ */
+public enum AssignmentType {
+    
+    FINAL, PROJECT, LAB, HOMEWORK;
+
+    /**
+     * Returns the AssignmentType corresponding to the string passed in.
+     *
+     * @param typeString
+     * @return corresponding AssignmentType
+     */
+    public static AssignmentType getInstance(String typeString) {
+        //Look for corresponding AssignmentType enum
+        for(AssignmentType type : values()) {
+            if(typeString.toUpperCase().equals(type.toString())) {
+                return type;
+            }
+        }
+
+        //TODO: Consider throwing a AssignmentTypeNotFound exception
+        //If not found, print error and return null
+        System.err.println("Invalid input: " + typeString +
+                            ", valid options are " + java.util.Arrays.toString(values()));
+        return null;
+    }
 }
