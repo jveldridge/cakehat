eb96510c5cc9fc22dd55e14115dd068527684c0e
alex.k.unger <alex.k.unger@414fe6b6-edb3-11de-91ba-91c70a792394>
2010-01-08 06:13:15 +0000

    adds unit tests for some DB methods (more to come) fix a few minor things brought up by unit test in DBWrapper

--- src/utils/DBWrapper.java
+++ src/utils/DBWrapper.java
@@ -20,18 +20,36 @@ import rubric.TimeStatus;
  */
 public class DBWrapper implements DatabaseIO {
 
-    private static Connection _connection;
-    private static Statement _statement;
+    private Connection _connection;
+    private Statement _statement;
+    private String _dbPath;
+
+    /**
+     * sets DB path to regular location
+     */
+    public DBWrapper() {
+        //should be set to constant
+        this("jdbc:sqlite:/Users/alexku/Documents/ Brown/sem5/cs015 ta/cakehat_db/cakehatcs015.sqlite");
+    }
+
+    /**
+     * for testing allows for custom path
+     * this constructor should only be used for testing the DBWrapper
+     * @param path - String path to DB file
+     */
+    public DBWrapper(String path) {
+        _dbPath = path;
+    }
 
     /**
      * opens a new connection to the DB
      * @param messageFrame - frame in which errors will show up
      */
-    private static void openConnection() {
+    private void openConnection() {
         _connection = null;
         try {
             Class.forName("org.sqlite.JDBC");
-            _connection = DriverManager.getConnection("jdbc:sqlite:/Users/alexku/EclipseWorkspace/grade_tests/lib/cakehatcs015.sqlite");
+            _connection = DriverManager.getConnection(_dbPath);
 
             _statement = _connection.createStatement();
             _statement.setQueryTimeout(30);  // set timeout to 30 sec.
@@ -46,7 +64,7 @@ public class DBWrapper implements DatabaseIO {
      * closes current connection to DB
      * @param messageFrame - frame in which errors will show up
      */
-    private static void closeConnection() {
+    private void closeConnection() {
         try {
             if (_connection != null) {
                 _connection.close();
@@ -62,9 +80,9 @@ public class DBWrapper implements DatabaseIO {
      * @param asgn - string of assignment name
      * @param distribution - hashmap of ta login to arraylist of student logins
      */
-    public void setAsgnDist(String asgn, Map<String, ArrayList<String>> distribution) {
+    public boolean setAsgnDist(String asgn, Map<String, ArrayList<String>> distribution) {
         //add the distribution to the DB
-        openConnection();
+        this.openConnection();
         try {
             HashMap<String, Integer> studentIDs = new HashMap<String, Integer>();
             HashMap<String, Integer> taIDs = new HashMap<String, Integer>();
@@ -79,26 +97,25 @@ public class DBWrapper implements DatabaseIO {
                 taIDs.put(rs.getString("login"), rs.getInt("tid"));
             }
             rs = _statement.executeQuery("SELECT a.aid FROM asgn AS a WHERE a.name == '" + asgn + "'");
-            asgnID = rs.getInt("tid");
+            asgnID = rs.getInt("aid");
 
-            _statement.executeQuery("DELETE FROM asgn WHERE name == '" + asgn + "'");
+            _statement.executeUpdate("DELETE FROM asgn WHERE name == '" + asgn + "'");
 
             String insertCommand = "INSERT INTO distribution ('aid', 'sid', 'tid') VALUES"; //start of insert command
             for (String ta : distribution.keySet()) {
                 ArrayList<String> distributedStudents = distribution.get(ta);
                 for (String student : distributedStudents) {
-                    insertCommand += " (" + asgnID + ", '" + student + "', '" + ta + "'),";
+                    _statement.addBatch(insertCommand + " (" + asgnID + ", '" + student + "', '" + ta + "')");
                 }
             }
-            if (insertCommand.endsWith(",")) { //cut off trailing comma
-                insertCommand = insertCommand.substring(0, insertCommand.length() - 1);
-            }
-            _statement.executeUpdate(insertCommand);
+            _statement.executeBatch();
             JOptionPane.showMessageDialog(null, "Assignments have been successfully distributed to the grading TAs.", "Success", JOptionPane.INFORMATION_MESSAGE);
-            closeConnection();
+            this.closeConnection();
+            return true;
         } catch (Exception e) {
             new ErrorView(e, "There was an error distributing the assignments to the grading TAs.");
-            closeConnection();
+            this.closeConnection();
+            return false;
         }
     }
 
@@ -109,25 +126,25 @@ public class DBWrapper implements DatabaseIO {
      * @return list of student logins that have been blacklisted by the ta
      */
     public Collection<String> getTABlacklist(String taLogin) {
-        openConnection();
+        this.openConnection();
         try {
             ArrayList<String> blackList = new ArrayList<String>();
 
             ResultSet rs = _statement.executeQuery("SELECT s.login "
                     + "FROM student AS s "
-                    + "INNER JOIN distribution AS d "
-                    + "ON d.sid == s.sid "
+                    + "INNER JOIN blacklist AS b "
+                    + "ON b.sid == s.sid "
                     + "INNER JOIN ta AS t "
-                    + "ON d.tid == t.tid "
+                    + "ON b.tid == t.tid "
                     + "WHERE t.login == '" + taLogin + "'");
             while (rs.next()) {
                 blackList.add(rs.getString("login"));
             }
-            closeConnection();
+            this.closeConnection();
             return blackList;
         } catch (Exception e) {
             new ErrorView(e, "Could not get black list for TA.");
-            closeConnection();
+            this.closeConnection();
             return null;
         }
     }
@@ -138,18 +155,18 @@ public class DBWrapper implements DatabaseIO {
      * @return status
      */
     public boolean addAssignment(String assignmentName) {
-        openConnection();
+        this.openConnection();
         try {
             _statement.executeUpdate("INSERT INTO asgn "
                     + "('name') "
                     + "VALUES "
                     + "('" + assignmentName
                     + "')");
-            closeConnection();
+            this.closeConnection();
             return true;
         } catch (Exception e) {
             new ErrorView(e, "Could not insert new row for assignment.");
-            closeConnection();
+            this.closeConnection();
             return false;
         }
     }
@@ -162,7 +179,7 @@ public class DBWrapper implements DatabaseIO {
      * @return status
      */
     public boolean addStudent(String studentLogin, String studentFirstName, String studentLastName) {
-        openConnection();
+        this.openConnection();
         try {
             _statement.executeUpdate("INSERT INTO student "
                     + "('login', 'firstname', 'lastname') "
@@ -171,11 +188,11 @@ public class DBWrapper implements DatabaseIO {
                     + "', '" + studentFirstName
                     + "', '" + studentLastName
                     + "')");
-            closeConnection();
+            this.closeConnection();
             return true;
         } catch (Exception e) {
             new ErrorView(e, "Could not insert new row for student.");
-            closeConnection();
+            this.closeConnection();
             return false;
         }
     }
@@ -186,18 +203,18 @@ public class DBWrapper implements DatabaseIO {
      * @return status
      */
     public boolean disableStudent(String studentLogin) {
-        openConnection();
+        this.openConnection();
         try {
             _statement.executeUpdate("UPDATE student "
                     + "SET "
                     + "enabled = 0 "
                     + "WHERE "
                     + "login == '" + studentLogin + "'");
-            closeConnection();
+            this.closeConnection();
             return true;
         } catch (Exception e) {
             new ErrorView(e, "Could not update student " + studentLogin + " to be disabled.");
-            closeConnection();
+            this.closeConnection();
             return false;
         }
     }
@@ -208,18 +225,18 @@ public class DBWrapper implements DatabaseIO {
      * @return status
      */
     public boolean enableStudent(String studentLogin) {
-        openConnection();
+        this.openConnection();
         try {
             _statement.executeUpdate("UPDATE student "
                     + "SET "
                     + "enabled = 1 "
                     + "WHERE "
                     + "login == '" + studentLogin + "'");
-            closeConnection();
+            this.closeConnection();
             return true;
         } catch (Exception e) {
             new ErrorView(e, "Could not update student " + studentLogin + " to be enabled.");
-            closeConnection();
+            this.closeConnection();
             return false;
         }
     }
@@ -233,7 +250,7 @@ public class DBWrapper implements DatabaseIO {
      * @return status
      */
     public boolean addTA(String taLogin, String taFirstName, String taLastName, String type) {
-        openConnection();
+        this.openConnection();
         try {
             ResultSet rs = _statement.executeQuery("SELECT COUNT(t.tid) AS rowcount "
                     + "FROM ta AS t "
@@ -249,17 +266,17 @@ public class DBWrapper implements DatabaseIO {
                         + "', '" + type
                         + "')");
             }
-            closeConnection();
+            this.closeConnection();
             return true;
         } catch (Exception e) {
             new ErrorView(e, "Could not insert new row for ta: " + taLogin);
-            closeConnection();
+            this.closeConnection();
             return false;
         }
     }
 
     public Map<String, String> getStudents() {
-        openConnection();
+        this.openConnection();
         try {
             ResultSet rs = _statement.executeQuery("SELECT s.login AS studlogin, " +
                                                     "s.firstname AS fname, " +
@@ -270,11 +287,11 @@ public class DBWrapper implements DatabaseIO {
             while (rs.next()) {
                 result.put(rs.getString("studlogin"), rs.getString("fname") + " " + rs.getString("lname"));
             }
-            closeConnection();
+            this.closeConnection();
             return result;
         } catch (Exception e) {
             new ErrorView(e, "Could not get All Enabled Students from DB");
-            closeConnection();
+            this.closeConnection();
             return new HashMap<String, String>();
         }
     }
@@ -286,12 +303,12 @@ public class DBWrapper implements DatabaseIO {
      * @return status
      */
     public boolean blacklistStudent(String studentLogin, String taLogin) {
-        openConnection();
+        this.openConnection();
         try {
             ResultSet rs = _statement.executeQuery("SELECT COUNT(b.sid) AS rowcount, "
                     + "b.tid AS taid, "
                     + "b.sid AS studentid "
-                    + "FROM backlist AS b "
+                    + "FROM blacklist AS b "
                     + "INNER JOIN student AS s "
                     + "ON s.sid == b.sid "
                     + "INNER JOIN ta AS t "
@@ -309,11 +326,11 @@ public class DBWrapper implements DatabaseIO {
                         + ", " + taID
                         + ")");
             }
-            closeConnection();
+            this.closeConnection();
             return true;
         } catch (Exception e) {
             new ErrorView(e, "Could not blacklist the student: " + studentLogin + " for ta: " + taLogin);
-            closeConnection();
+            this.closeConnection();
             return false;
         }
     }
@@ -324,7 +341,7 @@ public class DBWrapper implements DatabaseIO {
      * @return Boolean - Empty = true, Not = false
      */
     public boolean isDistEmpty(String asgn) {
-        openConnection();
+        this.openConnection();
         try {
             ResultSet rs = _statement.executeQuery("SELECT COUNT(d.sid) AS rowcount " +
                                                    "FROM distribution AS d " +
@@ -332,11 +349,11 @@ public class DBWrapper implements DatabaseIO {
                                                     "ON a.aid == d.aid " +
                                                    "WHERE a.name == '" + asgn + "'");
             int rows = rs.getInt("rowcount");
-            closeConnection();
+            this.closeConnection();
             return rows == 0;
         } catch (Exception e) {
             new ErrorView(e, "Could not determine the size of the dist for: " + asgn);
-            closeConnection();
+            this.closeConnection();
             return false;
         }
     }
@@ -346,7 +363,7 @@ public class DBWrapper implements DatabaseIO {
      * @return Collection - strings of all the student logins
      */
     public Collection<String> getBlacklistedStudents() {
-        openConnection();
+        this.openConnection();
         try {
             ResultSet rs = _statement.executeQuery("SELECT s.login AS studlogin " +
                                                    "FROM student AS s " +
@@ -356,11 +373,11 @@ public class DBWrapper implements DatabaseIO {
             while (rs.next()) {
                 result.add(rs.getString("studlogin"));
             }
-            closeConnection();
+            this.closeConnection();
             return result;
         } catch (Exception e) {
             new ErrorView(e, "Could not get BlackListed Students from DB");
-            closeConnection();
+            this.closeConnection();
             return new ArrayList<String>();
         }
     }
@@ -381,7 +398,7 @@ public class DBWrapper implements DatabaseIO {
      * @return Collection - list of student logins
      */
     public Collection<String> getStudentsAssigned(String assignmentName, String taLogin) {
-        openConnection();
+        this.openConnection();
         try {
             ResultSet rs = _statement.executeQuery("SELECT s.login AS studlogin " +
                                                    "FROM student AS s " +
@@ -397,11 +414,11 @@ public class DBWrapper implements DatabaseIO {
             while (rs.next()) {
                 result.add(rs.getString("studlogin"));
             }
-            closeConnection();
+            this.closeConnection();
             return result;
         } catch (Exception e) {
             new ErrorView(e, "Could not get students assigned to ta: " + taLogin + " for assignment: " + assignmentName);
-            closeConnection();
+            this.closeConnection();
             return new ArrayList<String>();
         }
     }
