e024bffb3777a9968c8eaff9ae75bbe87dd91833
nonother <nonother@414fe6b6-edb3-11de-91ba-91c70a792394>
2010-01-20 02:34:13 +0000

    Removes empty histogram package. Makes AssignmentComparator use generics.

--- trunk/src/utils/AssignmentComparator.java
+++ trunk/src/utils/AssignmentComparator.java
@@ -4,16 +4,12 @@ import config.Assignment;
 import java.util.Comparator;
 
 /**
- * AssignmentComparator compares assignments by their numbers
+ * AssignmentComparator compares assignments by their numbers.
  * 
  * @author jeldridg
  */
-public class AssignmentComparator implements Comparator{
-
-    public int compare(Object o1, Object o2) {
-                Assignment a1 = (Assignment) o1;
-                Assignment a2 = (Assignment) o2;
-                return ((Integer)a1.getNumber()).compareTo(a2.getNumber());
-            }
-    
+public class AssignmentComparator implements Comparator<Assignment> {
+    public int compare(Assignment a1, Assignment a2) {
+        return ((Integer)a1.getNumber()).compareTo(a2.getNumber());
+    }
 }
