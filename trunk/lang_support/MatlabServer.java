e722367ed2685f6273627a8f092d573c3fb65baf
stephenpoletto <stephenpoletto@414fe6b6-edb3-11de-91ba-91c70a792394>
2010-01-25 22:21:26 +0000

    adds matlab support files

---
+++ trunk/lang_support/MatlabServer.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.net.*;
+import java.util.StringTokenizer;
+
+public class MatlabServer 
+{
+  MatlabControl _controller;
+
+  public MatlabServer() {
+	_controller = new MatlabControl();
+	try {
+		this.setup();
+	} catch(IOException e) {
+		System.out.println(e.toString());
+	}
+  }
+  
+  public void setup() throws IOException {
+	class Caller extends Thread {
+		public void run() {
+			try {
+				listen();
+			}
+			catch (IOException e) {
+				System.out.println(e.toString());
+			}
+		}
+		public void listen() throws IOException {
+			ServerSocket sock = null;
+			boolean isListening = true;
+			try {
+				sock = new ServerSocket(4444);
+			}
+			catch (IOException e) {
+				System.out.println(e.toString());
+			}
+			while(isListening) {
+				new ControlThread(sock.accept()).run();
+			}
+			sock.close();
+		}
+	}
+	Caller c = new Caller();
+	c.start();
+  }
+
+  public class ControlThread {
+    
+	private Socket _sock = null;
+
+    	public ControlThread(Socket socket) {
+		_sock = socket;
+	}
+
+	public void run() {
+		try {
+			PrintWriter out = new PrintWriter(_sock.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(_sock.getInputStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) { //while there are still lines to exec
+				processInput(inputLine);
+			}
+			out.close();
+			in.close();
+			_sock.close();
+		} catch (IOException e) {
+			System.out.println(e.toString());
+		}
+	}
+
+	public void processInput(String cmd) {
+		try {
+			_controller.eval(cmd);
+		}
+		catch(Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+  }
+}
+
