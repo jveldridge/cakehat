a47606235503f37706d25a1fba11c7a790da3bbc
stephenpoletto <stephenpoletto@414fe6b6-edb3-11de-91ba-91c70a792394>
2010-01-26 05:14:28 +0000

    matlab support package "finished"

--- trunk/src/matlab/MatlabClient.java
+++ trunk/src/matlab/MatlabClient.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package matlab;
 
 import utils.*;
@@ -10,39 +5,41 @@ import java.io.*;
 import java.net.*;
 
 /**
+ * Client used to communicate with the MATLAB server to send
+ * commands to MATLAB. Whenever cakehat needs to send a command
+ * to MATLAB, it does so through this class.
  *
  * @author spoletto
  */
- public class MatlabClient {
 
-        private Socket _sock;
-        private PrintWriter _out;
+public class MatlabClient {
 
-        public MatlabClient() {
-            _sock = null;
-            _out  = null;
-             try {
-                    _sock = new Socket("localhost", 4444);
-                    _out = new PrintWriter(_sock.getOutputStream(), true);
-                } catch (UnknownHostException e) {
-                new ErrorView(e, "Could not bind socket to localhost"); }
-                catch (IOException e) {
-                new ErrorView(e, "Could not bind socket to localhost"); }
+    private Socket _sock;
+    private PrintWriter _out;
+
+    public MatlabClient() {
+        _sock = null;
+        _out = null;
+        try {
+            _sock = new Socket("localhost", 4444);
+            _out = new PrintWriter(_sock.getOutputStream(), true);
+        } catch (UnknownHostException e) {
+            new ErrorView(e, "Could not bind socket to localhost");
+        } catch (IOException e) {
+            new ErrorView(e, "Could not bind socket to localhost");
         }
-
-        public void sendCommand(String cmd) {
-            _out.println(cmd);
-        }
-
-        public void die() {
-            try {
-                _sock.close();
-            }
-            catch (IOException e) {
-                new ErrorView(e, "Could not kill client");
-            }
-            _out.close();
-        }
-
-
     }
+
+    public void sendCommand(String cmd) {
+        _out.println(cmd);
+    }
+
+    public void die() {
+        try {
+            _sock.close();
+        } catch (IOException e) {
+            new ErrorView(e, "Could not kill client");
+        }
+        _out.close();
+    }
+}
