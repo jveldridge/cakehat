b8e671bf3a7c87658dd31e326b459ac10a990a4c
nonother <nonother@414fe6b6-edb3-11de-91ba-91c70a792394>
2010-01-27 21:27:44 +0000

    More support for modes in CustomHandin. Adds "specify-user-script" mode for RUN and TESTER that when executing the command (as specified by the property) replace all instances of "^student_login^" with the login actually being used. Does not support opening or printing code because there's currently no way to specify file extensions.

--- trunk/src/config/CustomHandin.java
+++ trunk/src/config/CustomHandin.java
@@ -11,19 +11,25 @@ import utils.BashConsole;
 class CustomHandin extends CodeHandin
 {
     private static final String
-    SPECIFY_SCRIPT = "specify-script", COMMAND="command";
+    SPECIFY_SCRIPT = "specify-script",
+    SPECIFY_USER_SCRIPT = "specify-user-script", STUDENT_LOGIN_TAG = "^student_login^",
+    COMMAND="command";
 
     //Mode
     private static final LanguageSpecification.Mode
     SPECIFY_SCRIPT_MODE = new LanguageSpecification.Mode(SPECIFY_SCRIPT,
-                            new LanguageSpecification.Property(COMMAND, true));
+                              new LanguageSpecification.Property(COMMAND, true)),
+    //In this mode any occurence of ^student_login^ in the command will be replaced
+    //by the applicable student login that run or tester is being called for
+    SPECIFY_USER_SCRIPT_MODE = new LanguageSpecification.Mode(SPECIFY_USER_SCRIPT,
+                                   new LanguageSpecification.Property(COMMAND, true));
 
     //Language specification
     public static final LanguageSpecification SPECIFICATION =
     new LanguageSpecification("custom",
+                             new LanguageSpecification.Mode[]{ SPECIFY_SCRIPT_MODE, SPECIFY_USER_SCRIPT_MODE },
                              new LanguageSpecification.Mode[]{ SPECIFY_SCRIPT_MODE },
-                             new LanguageSpecification.Mode[]{ SPECIFY_SCRIPT_MODE },
-                             new LanguageSpecification.Mode[]{ SPECIFY_SCRIPT_MODE });
+                             new LanguageSpecification.Mode[]{ SPECIFY_SCRIPT_MODE, SPECIFY_USER_SCRIPT_MODE });
 
     CustomHandin(Assignment asgn, String name, int points)
     {
@@ -37,20 +43,49 @@ class CustomHandin extends CodeHandin
     }
 
     @Override
+    public boolean hasOpen()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean hasPrint()
+    {
+        return false;
+    }
+
+    @Override
     public void run(String studentLogin)
     {
-        BashConsole.writeThreaded(this.getRunProperty(COMMAND));
+        if(_runMode.equalsIgnoreCase(SPECIFY_SCRIPT))
+        {
+            BashConsole.writeThreaded(this.getRunProperty(COMMAND));
+        }
+        else if(_runMode.equalsIgnoreCase(SPECIFY_USER_SCRIPT))
+        {
+            BashConsole.writeThreaded(this.getRunProperty(COMMAND.replaceAll(STUDENT_LOGIN_TAG, studentLogin)));
+        }
     }
 
     @Override
     public void runDemo()
     {
-        BashConsole.writeThreaded(this.getDemoProperty(COMMAND));
+        if(_demoMode.equalsIgnoreCase(SPECIFY_SCRIPT))
+        {
+            BashConsole.writeThreaded(this.getDemoProperty(COMMAND));
+        }
     }
 
     @Override
     public void runTester(String studentLogin)
     {
-        BashConsole.writeThreaded(this.getTesterProperty(COMMAND));
+        if(_testerMode.equalsIgnoreCase(SPECIFY_SCRIPT))
+        {
+            BashConsole.writeThreaded(this.getTesterProperty(COMMAND));
+        }
+        else if(_testerMode.equalsIgnoreCase(SPECIFY_USER_SCRIPT))
+        {
+            BashConsole.writeThreaded(this.getTesterProperty(COMMAND.replaceAll(STUDENT_LOGIN_TAG, studentLogin)));
+        }
     }
 }
