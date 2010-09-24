e3ebc7ffb0308b87cb614f21ea2e995534467265
nonother <nonother@414fe6b6-edb3-11de-91ba-91c70a792394>
2010-09-24 15:13:39 +0000

    merge r534 into trunk : http://cakehat.cs.brown.edu/foodcritic/r/62/

--- trunk/src/gradesystem/GradeSystemApp.java
+++ trunk/src/gradesystem/GradeSystemApp.java
@@ -2,6 +2,10 @@ package gradesystem;
 
 import backend.BackendView;
 import frontend.FrontendView;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import org.jdesktop.application.Application;
 import org.jdesktop.application.SingleFrameApplication;
@@ -12,11 +16,8 @@ import org.jdesktop.application.SingleFrameApplication;
 public class GradeSystemApp extends SingleFrameApplication
 {
     private static String[] _args;
-    private static boolean testing;
-
-    public static boolean inTestMode() {
-        return testing;
-    }
+    private static final String BACKEND_ARG = "backend";
+    private static final String FRONTEND_ARG = "frontend";
 
     /**
      * At startup create and show the main frame of the application.
@@ -26,24 +27,82 @@ public class GradeSystemApp extends SingleFrameApplication
     {
         this.applyDisplaySettings();
 
-        if (_args.length >= 1)
+        Runnable initialWindowCommand;
+        if (inTestMode())
         {
-            if (_args[0].compareToIgnoreCase("backend") == 0)
+            initialWindowCommand = new Runnable()
             {
-                BackendView.launch();
-            }
-            else if (_args[0].compareToIgnoreCase("frontend") == 0)
+                public void run()
+                {
+                    show(new GradeSystemView(GradeSystemApp.this));
+                }
+            };
+        }
+        else if (_args[0].equalsIgnoreCase(BACKEND_ARG))
+        {
+            initialWindowCommand = new Runnable()
             {
-                FrontendView.launch();
-            }
-            testing = false;
+                public void run()
+                {
+                    BackendView.launch();
+                }
+            };
+        }
+        else if (_args[0].equalsIgnoreCase(FRONTEND_ARG))
+        {
+            initialWindowCommand = new Runnable()
+            {
+                public void run()
+                {
+                    FrontendView.launch();
+                }
+            };
         }
         else
         {
-            testing = true;
-            show(new GradeSystemView(this));
+            initialWindowCommand = new Runnable()
+            {
+                public void run()
+                {
+                    JOptionPane.showMessageDialog(null, "Invalid run arg: " + _args[0]);
+                }
+            };
         }
 
+        if (isUsingSSH())
+        {
+            invokeAfterSwingLaunch(initialWindowCommand);
+        }
+        else
+        {
+            initialWindowCommand.run();
+        }
+    }
+
+    /**
+     * Invokes the <code>action</code> after Swing has initialized. Swing is
+     * initialized by creating a small empty <code>JFrame</code> and then
+     * disposing it after the action is invokes.
+     *
+     * NOTE: This is a hack to solve the SSH/Swing issue.
+     *
+     * @param action
+     */
+    private static void invokeAfterSwingLaunch(final Runnable action)
+    {
+        final JFrame loadingFrame = new JFrame("cakehat loading");
+
+        loadingFrame.addWindowListener(new WindowAdapter()
+        {
+            public void windowOpened(WindowEvent e)
+            {
+                action.run();
+                loadingFrame.dispose();
+            }
+        });
+
+        loadingFrame.setLocationRelativeTo(null);
+        loadingFrame.setVisible(true);
     }
 
     /**
@@ -58,19 +117,18 @@ public class GradeSystemApp extends SingleFrameApplication
             {
                 try
                 {
-                    //If not ssh
-                    if ( (_args.length >= 2 && _args[1].compareToIgnoreCase("ssh") != 0)
-                              || _args.length < 2)
-                    {
-                        UIManager.setLookAndFeel(new javax.swing.plaf.metal.MetalLookAndFeel());
-                    }
-                    //If ssh
-                    else
+                    if(isUsingSSH())
                     {
                         System.setProperty("awt.useSystemAAFontSettings", "false");
                         System.setProperty("swing.aatext", "false");
                     }
+                    else
+                    {
+                        UIManager.setLookAndFeel(new javax.swing.plaf.metal.MetalLookAndFeel());
+                    }
                 }
+                //Setting the look and feel may fail, if so the program will most
+                //likely still operate correctly but may display slightly oddly
                 catch (Exception e) { }
             }
         });
@@ -91,6 +149,16 @@ public class GradeSystemApp extends SingleFrameApplication
     public static GradeSystemApp getApplication()
     {
         return Application.getInstance(GradeSystemApp.class);
+    }
+
+    public static boolean isUsingSSH()
+    {
+        return _args.length >= 2 && _args[1].equalsIgnoreCase("ssh");
+    }
+
+    public static boolean inTestMode()
+    {
+        return _args.length == 0;
     }
 
     /**
