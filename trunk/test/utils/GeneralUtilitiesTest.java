9036dbfb0b30098e1bd976e58fef296ba122775e
hannah.d.rosen@gmail.com <hannah.d.rosen@gmail.com@414fe6b6-edb3-11de-91ba-91c70a792394>
2010-11-16 04:55:51 +0000

    resolves issue none: Splits GeneralUtilitiesTest class: splits the GeneralUtilitiesTest class to match the splitting of the GeneralUtilities class into smaller utilities classes: tests sill pass: http://cakehat.cs.brown.edu/foodcritic/r/122

---
+++ trunk/test/utils/GeneralUtilitiesTest.java
@@ -0,0 +1,36 @@
+/**This class contains methods that test the methods in the GeneralUtilities class.
+ *
+ * @author hdrosen
+ */
+
+package utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GeneralUtilitiesTest {
+
+    private GeneralUtilities _instance;
+
+    public GeneralUtilitiesTest() {
+        _instance = new GeneralUtilities();
+    }
+
+    
+    @Test
+    public void testDoubleToString() {
+        assertEquals("3.35", _instance.doubleToString(3.34999));
+        assertEquals("1.25", _instance.doubleToString(1.25));
+        assertEquals("1.0", _instance.doubleToString(1));
+        assertEquals("-1.25", _instance.doubleToString(-1.25));
+    }
+
+    @Test
+    public void testRound() {
+        assertEquals(3.3, _instance.round(3.25, 1), 0);
+        assertEquals(3.3, _instance.round(3.32, 1), 0);
+        assertEquals(3.32, _instance.round(3.32, 2), 0);
+        assertEquals(19, _instance.round(18.95432, 0), 0);
+    }
+
+}
