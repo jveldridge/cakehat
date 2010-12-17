5c7a4ace0144e932d6831efd57d358a7ce146019
nonother <nonother@414fe6b6-edb3-11de-91ba-91c70a792394>
2010-12-17 22:26:06 +0000

    Next step in refactoring code base. Removes old packages.

---
+++ test/gradesystem/utils/UserUtilitiesTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gradesystem.utils;
+
+import gradesystem.utils.UserUtilities;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hdrosen
+ */
+public class UserUtilitiesTest {
+
+    private UserUtilities _instance;
+
+    public UserUtilitiesTest() {
+        _instance = new UserUtilities();
+    }
+
+//This tests the getUserName method in the UserUtilities class.
+    @Test
+    public void testUserName() {
+        assertEquals("Ashley Rose Tuccero", _instance.getUserName("ashley"));
+        assertEquals("UNKNOWN_LOGIN", _instance.getUserName("hello"));
+
+    }
+
+}
