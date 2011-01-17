b3f7f86cb49a8c39c913887fdc2558d073e0f9e4
nonother <nonother@414fe6b6-edb3-11de-91ba-91c70a792394>
2011-01-17 21:14:16 +0000

    Issue 233 : MATLAB Demo Action : Adds a MATLAB demo action. The action is very similar to the MATLAB run mode with a few differences: a directory must be specified to find the demos in, and if a file to run is specified and does not exist then the user will be notified (but unlike the run action - not able to select from all available m-files). : Tested against Battleship. Able to correctly handle all error cases and to run properly when the properties were valid.

--- src/gradesystem/handin/MatlabActions.java
+++ src/gradesystem/handin/MatlabActions.java
@@ -48,6 +48,8 @@ class MatlabActions implements ActionProvider
     {
         ArrayList<DistributableActionDescription> descriptions =
                 new ArrayList<DistributableActionDescription>();
+
+        descriptions.add(new DemoFile());
         descriptions.add(new OpenFiles());
         descriptions.add(new RunFile());
 
@@ -57,8 +59,137 @@ class MatlabActions implements ActionProvider
     //TODO: Action that copies in content of config-specified directory into
     //handin and then runs a config-specified file in MATLAB
 
-    //TODO: Demo action(s). Definitely have action that runs a specified m-file.
-    //Have a demo mode that allows for selecting which file m-file to run?
+    private class DemoFile implements DistributableActionDescription
+    {
+        private final DistributableActionProperty DIRECTORY_PATH_PROPERTY =
+            new DistributableActionProperty("directory-path",
+            "Specifies the absolute path to the directory containing the m-file(s) " +
+            "that make up the demo. All files in the specified directory and " +
+            "sub-directories are considered included.", true);
+
+        private final DistributableActionProperty FILE_PATH_PROPERTY =
+            new DistributableActionProperty("file-path",
+            "Specifies the path relative to DIRECTORY_PATH_PROPERTY. " +
+            "If this property is not specified then the grader will be asked " +
+            "to select which file to run.", false);
+
+        public ActionProvider getProvider()
+        {
+            return MatlabActions.this;
+        }
+
+        public String getName()
+        {
+            return "demo-file";
+        }
+
+        public String getDescription()
+        {
+            return "Runs a single m-file with grader provided arguments. The " +
+                    "grader will either be allowed to select which m-file to run " +
+                    "from a list of all m-files in the demo, or if FILE_PATH_PROPERTY " +
+                    "is specified then only the file specified by that property.";
+        }
+
+        public List<DistributableActionProperty> getProperties()
+        {
+            return Arrays.asList(new DistributableActionProperty[] { DIRECTORY_PATH_PROPERTY,
+                FILE_PATH_PROPERTY });
+        }
+
+        public List<ActionMode> getSuggestedModes()
+        {
+            return Arrays.asList(new ActionMode[] { ActionMode.DEMO });
+        }
+
+        public List<ActionMode> getCompatibleModes()
+        {
+            return Arrays.asList(new ActionMode[] { ActionMode.DEMO,
+                ActionMode.RUN, ActionMode.TEST, ActionMode.OPEN });
+        }
+
+        public DistributableAction getAction(final Map<DistributableActionProperty, String> properties)
+        {
+            DistributableAction action = new StandardDistributableAction()
+            {
+                public void performAction(DistributablePart part, Group group) throws ActionException
+                {
+                    //Ensure specified directory is valid
+                    File demoDir = new File(properties.get(DIRECTORY_PATH_PROPERTY));
+                    if(!demoDir.exists())
+                    {
+                        JOptionPane.showMessageDialog(null,
+                                "Directory specified by '" +
+                                DIRECTORY_PATH_PROPERTY.getName() +
+                                "' does not exist.\n" +
+                                "Directory: " + demoDir.getAbsolutePath(),
+                                "Directory does not exist",
+                                JOptionPane.WARNING_MESSAGE);
+                        return;
+                    }
+                    if(!demoDir.isDirectory())
+                    {
+                        JOptionPane.showMessageDialog(null,
+                                "Directory specified by '" +
+                                DIRECTORY_PATH_PROPERTY.getName() +
+                                "' is not a directory.\n" +
+                                "Directory: " + demoDir.getAbsolutePath(),
+                                "Not a directory",
+                                JOptionPane.WARNING_MESSAGE);
+                        return;
+                    }
+                    
+                    //Build list of m-files that are to be demo
+                    List<File> mFiles;
+
+                    //If a specific file to run was specified, attempt to demo
+                    //that file, but if it does not exist inform and abort
+                    if(properties.containsKey(FILE_PATH_PROPERTY))
+                    {
+                        String relativePath = properties.get(FILE_PATH_PROPERTY);
+                        File absolutePath = new File(demoDir, relativePath);
+
+                        if(!absolutePath.exists())
+                        {
+                            JOptionPane.showMessageDialog(null,
+                                    "File specified by '" +
+                                    FILE_PATH_PROPERTY.getName() +
+                                    "' does not exist.\n" +
+                                    "File: " +
+                                    absolutePath.getAbsolutePath(),
+                                    "File does not exist",
+                                    JOptionPane.WARNING_MESSAGE);
+                            return;
+                        }
+                        else
+                        {
+                            mFiles = new ArrayList<File>();
+                            mFiles.add(absolutePath);
+                        }
+                    }
+                    else
+                    {
+                        mFiles = Allocator.getFileSystemUtilities()
+                                .getFiles(demoDir, new FileExtensionFilter("m"),
+                                new AlphabeticFileComparator());
+                    }
+
+                    if(mFiles.isEmpty())
+                    {
+                        JOptionPane.showMessageDialog(null,
+                                "There are no m-files to demo.",
+                                "Unable to demo", JOptionPane.WARNING_MESSAGE);
+                    }
+                    else
+                    {
+                        runMFiles(mFiles, demoDir);
+                    }
+                }
+            };
+
+            return action;
+        }
+    }
 
     private class RunFile implements DistributableActionDescription
     {
@@ -82,9 +213,10 @@ class MatlabActions implements ActionProvider
         public String getDescription()
         {
             return "Runs a single m-file with grader provided arguments. The " +
-                    "grader will be allowed to select which m-file to run from " +
-                    "a list of all m-files that are part of this distributable " +
-                    "part.";
+                    "grader will either be allowed to select which m-file to run " +
+                    "from a list of all m-files that are part of this distributable " +
+                    "part or the grader will only be able to run the m-file " +
+                    "specified by FILE_PATH_PROPERTY.";
         }
 
         public List<DistributableActionProperty> getProperties()
@@ -150,104 +282,16 @@ class MatlabActions implements ActionProvider
                         JOptionPane.showMessageDialog(null,
                                 "There are no m-files for this distributable part.",
                                 "Unable to run", JOptionPane.WARNING_MESSAGE);
-                        return;
                     }
-
-                    //Create paths that are relative to the unarchive directory
-                    //These paths will be dislayed to the grader
-                    ArrayList<String> relativePaths = new ArrayList<String>();
-                    final HashMap<String, File> relativeToAbsolute = new HashMap<String, File>();
-                    for(File mFile : mFiles)
+                    else
                     {
-                        String relativePath = mFile.getAbsolutePath();
-                        relativePath = relativePath.replace(unarchiveDir.getAbsolutePath(), "");
-                        relativeToAbsolute.put(relativePath, mFile);
-                        relativePaths.add(relativePath);
-                    }
-
-                    //Create a GUI for the grader to select which m-file to run
-                    JPanel panel = new JPanel();
-                    panel.setLayout(new GridLayout(0,1));
-
-                    final GenericJComboBox<String> functionsComboBox = new GenericJComboBox<String>(relativePaths);
-                    final JTextField argumentsField = new ShadowJTextField("Function arguments (optional)");
-                    final JLabel evalLabel = new JLabel("");
-
-                    panel.add(functionsComboBox);
-                    panel.add(argumentsField);
-                    panel.add(evalLabel);
-
-                    //Update the evalLabel when either the argumentsField is
-                    //updated or a new selection from functionComboBox is made
-                    final Runnable updateEval = new Runnable()
-                    {
-                        public void run()
-                        {
-                            File mFile = relativeToAbsolute.get(functionsComboBox.getSelectedItem());
-                            String evalCmd = fileToFunctionName(mFile);
-                            //Add function arguments if provided
-                            if(argumentsField.getText() != null && !argumentsField.getText().isEmpty())
-                            {
-                                evalCmd += "(" + argumentsField.getText() + ")";
-                            }
-                            evalLabel.setText(evalCmd);
-                        }
-                    };
-                    argumentsField.getDocument().addDocumentListener(new DocumentListener()
-                    {
-                        public void insertUpdate(DocumentEvent de) { updateEval.run(); }
-                        public void removeUpdate(DocumentEvent de) { updateEval.run(); }
-                        public void changedUpdate(DocumentEvent de){ updateEval.run(); }
-                    });
-                    functionsComboBox.addActionListener(new ActionListener()
-                    {
-                        public void actionPerformed(ActionEvent ae) { updateEval.run(); }
-                    });
-                    updateEval.run();
-
-                    int result = JOptionPane.showConfirmDialog(null, panel,
-                            "Run m-file", JOptionPane.OK_CANCEL_OPTION);
-                    if(result == JOptionPane.OK_OPTION)
-                    {
-                        try
-                        {
-                            RemoteMatlabProxy proxy = getMatlabProxy();
-                            
-                            //Move to parent file's directory
-                            String parentDir = relativeToAbsolute.get(functionsComboBox.getSelectedItem()).getParent();
-                            try
-                            {
-                                proxy.eval("cd " + parentDir);
-                            }
-                            catch(MatlabInvocationException e)
-                            {
-                                throw new ActionException("Unable to make MATLAB change directory: " + parentDir, e);
-                            }
-
-                            //An exception in this case is likely due to invalid arguments
-                            //supplied to the MATLAB function
-                            try
-                            {
-                                proxy.eval(evalLabel.getText());
-                            }
-                            catch(MatlabInvocationException e)
-                            {
-                                JOptionPane.showMessageDialog(null,
-                                        "Invalid MATLAB command, please see MATLAB for more information.",
-                                        "Invalid MATLAB Command",
-                                        JOptionPane.WARNING_MESSAGE);
-                            }
-                        }
-                        catch(MatlabConnectionException e)
-                        {
-                            throw new ActionException(e);
-                        }
+                        runMFiles(mFiles, unarchiveDir);
                     }
                 }
             };
 
             return action;
-        }   
+        } 
     }
 
     private class OpenFiles implements DistributableActionDescription
@@ -370,8 +414,98 @@ class MatlabActions implements ActionProvider
         return _proxy;
     }
 
-    private String fileToFunctionName(File mfile)
+    private void runMFiles(List<File> mFiles, File containingDir) throws ActionException
     {
-        return mfile.getName().split("\\.")[0];
+        //Create paths that are relative to the unarchive directory
+        //These paths will be dislayed to the grader
+        ArrayList<String> relativePaths = new ArrayList<String>();
+        final HashMap<String, File> relativeToAbsolute = new HashMap<String, File>();
+        for(File mFile : mFiles)
+        {
+            String relativePath = mFile.getAbsolutePath();
+            relativePath = relativePath.replace(containingDir.getAbsolutePath(), "");
+            relativeToAbsolute.put(relativePath, mFile);
+            relativePaths.add(relativePath);
+        }
+
+        //Create a GUI for the grader to select which m-file to run
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(0,1));
+
+        final GenericJComboBox<String> functionsComboBox = new GenericJComboBox<String>(relativePaths);
+        final JTextField argumentsField = new ShadowJTextField("Function arguments (optional)");
+        final JLabel evalLabel = new JLabel("");
+
+        panel.add(functionsComboBox);
+        panel.add(argumentsField);
+        panel.add(evalLabel);
+
+        //Update the evalLabel when either the argumentsField is
+        //updated or a new selection from functionComboBox is made
+        final Runnable updateEval = new Runnable()
+        {
+            public void run()
+            {
+                File mFile = relativeToAbsolute.get(functionsComboBox.getSelectedItem());
+                //Convert file name to functionName
+                String evalCmd = mFile.getName().split("\\.")[0];
+                //Add function arguments if provided
+                if(argumentsField.getText() != null && !argumentsField.getText().isEmpty())
+                {
+                    evalCmd += "(" + argumentsField.getText() + ")";
+                }
+                evalLabel.setText(evalCmd);
+            }
+        };
+        argumentsField.getDocument().addDocumentListener(new DocumentListener()
+        {
+            public void insertUpdate(DocumentEvent de) { updateEval.run(); }
+            public void removeUpdate(DocumentEvent de) { updateEval.run(); }
+            public void changedUpdate(DocumentEvent de){ updateEval.run(); }
+        });
+        functionsComboBox.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent ae) { updateEval.run(); }
+        });
+        updateEval.run();
+
+        int result = JOptionPane.showConfirmDialog(null, panel,
+                "Run m-file", JOptionPane.OK_CANCEL_OPTION);
+        if(result == JOptionPane.OK_OPTION)
+        {
+            try
+            {
+                RemoteMatlabProxy proxy = getMatlabProxy();
+
+                //Move to parent file's directory
+                String parentDir = relativeToAbsolute.get(functionsComboBox.getSelectedItem()).getParent();
+                try
+                {
+                    proxy.eval("cd " + parentDir);
+                }
+                catch(MatlabInvocationException e)
+                {
+                    throw new ActionException("Unable to make MATLAB change directory: " + parentDir, e);
+                }
+
+                //An exception in this case is likely due to invalid arguments
+                //supplied to the MATLAB function
+                try
+                {
+                    proxy.eval(evalLabel.getText());
+                }
+                catch(MatlabInvocationException e)
+                {
+                    JOptionPane.showMessageDialog(null,
+                            "Invalid MATLAB command, please see MATLAB for more information.",
+                            "Invalid MATLAB Command",
+                            JOptionPane.WARNING_MESSAGE);
+                }
+            }
+            catch(MatlabConnectionException e)
+            {
+                throw new ActionException(e);
+            }
+        }
     }
 }
