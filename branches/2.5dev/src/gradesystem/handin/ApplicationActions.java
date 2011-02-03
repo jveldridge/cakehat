bdb2a97d7d2fd9ed55346149b11c90e3a39afc4d
nonother <nonother@414fe6b6-edb3-11de-91ba-91c70a792394>
2011-02-03 06:06:44 +0000

    Issue None : MATLAB actions compatible with paths with spaces : Makes MATLAB actions compatible with spaces by switching from eval-ing the path arguments (essentially like using a console) to feval-ing the path arguments (like calling a function). In this manner spaces in strings are not problematic because strings are kept whole - not separated on spaces. : Confirmed that code can be run, tested, demoed, and opened properly on assignment's that have part names which have spaces in them. : http://cakehat.cs.brown.edu/foodcritic/r/207/

--- branches/2.5dev/src/gradesystem/handin/ApplicationActions.java
+++ branches/2.5dev/src/gradesystem/handin/ApplicationActions.java
@@ -8,7 +8,6 @@ import java.io.FileFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
