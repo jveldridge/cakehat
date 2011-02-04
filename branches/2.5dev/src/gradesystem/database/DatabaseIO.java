43fd8e8b0868bcc42f75d41a510c7be43f5e6961
alex.k.unger <alex.k.unger@414fe6b6-edb3-11de-91ba-91c70a792394>
2011-02-04 22:23:42 +0000

    revert accidental DatabaseIO check in

--- branches/2.5dev/src/gradesystem/database/DatabaseIO.java
+++ branches/2.5dev/src/gradesystem/database/DatabaseIO.java
@@ -1,6 +1,5 @@
 package gradesystem.database;
 
-import gradesystem.components.Student;
 import gradesystem.config.Assignment;
 import gradesystem.config.HandinPart;
 import gradesystem.config.Part;
@@ -75,25 +74,28 @@ public interface DatabaseIO {
     public Map<String,String> getEnabledStudents() throws SQLException;
 
     /**
-     * Adds all the students in students to the blacklist of the given TA, if
+     * Adds student with login studentLogin to the blacklist of the given TA, if
      * not already blacklisted.  This ensures that this TA will not be distributed
-     * this student to grade. If the student was already blacklisted it is not added
-     * again. If there was an error a SQLException will be thrown.
+     * this student to grade.  The return value indicates whether or not the student
+     * was in fact added to the TA's blacklist.
      *
-     * @param students
+     * @param studentLogin
      * @param taLogin
+     * @return true if the student was added to the TA's blacklist; false if the
+     *         student was already on the TA's blacklist and was thus not added
+     *         again
      */
-    public void blacklistStudents(Collection<Student> students, TA ta) throws SQLException;
+    public boolean blacklistStudent(String studentLogin, TA ta) throws SQLException;
 
     /**
-     * Removes students in studentLogins from the blacklist of the given TA,
+     * Removes student with login studentLogin from the blacklist of the given TA,
      * if the student was on that TA's blacklist.  If the student was not on the
      * TA's blacklist, this method has no effect.
      *
-     * @param studentLogins
+     * @param studentLogin
      * @param ta
      */
-    public void unBlacklistStudents(Collection<String> studentLogins, TA ta) throws SQLException;
+    public void unBlacklistStudent(String studentLogin, TA ta) throws SQLException;
 
     /**
      * Indicates whether the Assignment has a distribution.  Returns true if
