7a96efc0178c2c6cf318be65c2b0bc1e4fc04101
nonother <nonother@414fe6b6-edb3-11de-91ba-91c70a792394>
2011-02-27 21:56:57 +0000

    Resolves Issue None : Updates CakehatAboutBox to contain more useful information.

---
+++ branches/release3.0.1/src/gradesystem/CakehatReleaseInfo.java
@@ -0,0 +1,25 @@
+package gradesystem;
+
+/**
+ * All of the information is updated before each release. It is not updated
+ * during development.
+ *
+ * @author jak2
+ */
+public class CakehatReleaseInfo
+{
+    public static String getVersion()
+    {
+        return "3.0.1";
+    }
+
+    public static String getReleaseDate()
+    {
+        return "February 27, 2011";
+    }
+
+    public static String getReleaseCommitNumber()
+    {
+        return "r666";
+    }
+}
