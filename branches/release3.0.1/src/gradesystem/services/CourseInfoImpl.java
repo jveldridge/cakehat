c41a336b97bead05d3c78e9ecddf9ded05fa93fd
alex.k.unger <alex.k.unger@414fe6b6-edb3-11de-91ba-91c70a792394>
2011-03-28 02:01:12 +0000

    merges r708 into 3.0.1rel

--- branches/release3.0.1/src/gradesystem/services/CourseInfoImpl.java
+++ branches/release3.0.1/src/gradesystem/services/CourseInfoImpl.java
@@ -1,7 +1,10 @@
 package gradesystem.services;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
- * 
+ *
  * @author jak2
  */
 public class CourseInfoImpl implements CourseInfo
@@ -9,26 +12,17 @@ public class CourseInfoImpl implements CourseInfo
     //For testing purposes, specifies which course this is being run for
     private final static String TESTING_COURSE = "cs000";
 
-    String _course = null;
-    public String getCourse()
-    {
-        if(_course == null)
-        {
+    private String _course = null;
+    public String getCourse() {
+        if (_course == null) {
             //Get the location of where this code is running
             String loc = this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
 
-            //If this is actually the jar we are running from
-            if(loc.endsWith("jar") && loc.startsWith("/course/cs"))
-            {
-                String course = loc.replace("/course/", "");
-                course = course.substring(0, course.indexOf("/"));
-
-                return course;
-            }
-            else
-            {
+            String course = this.getCourseFromPath(loc);
+            if (course != null) {
+                _course = course;
+            } else {
                 System.out.println("Using hard-coded test value for course: " + TESTING_COURSE);
-
                 _course = TESTING_COURSE;
             }
         }
@@ -49,5 +43,19 @@ public class CourseInfoImpl implements CourseInfo
     public String getHTAGroup()
     {
         return getCourse() + "hta";
+    }
+
+    protected String getCourseFromPath(String path) {
+        /* matches: any characters followed by "/course/cs" and grabs "cs" and
+         * the 3 digits that follow with or without a trailing letter */
+        Pattern regex = Pattern.compile(".*?/course/(cs[0-9]{3}[a-z]?)/",
+                Pattern.CANON_EQ | Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+        Matcher regexMatcher = regex.matcher(path);
+        //If this is actually the jar we are running from
+        if (regexMatcher.find()) {
+            return regexMatcher.group(1);
+        } else {
+            return null;
+        }
     }
 }
