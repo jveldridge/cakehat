d3a98955ad77235c6fb74d69990823ec74eac29d
alex.k.unger <alex.k.unger@414fe6b6-edb3-11de-91ba-91c70a792394>
2011-03-29 22:57:44 +0000

    merges rest of 2.5dev back into trunk exclamation mark

--- test/gradesystem/services/UserServicesTest.java
+++ test/gradesystem/services/UserServicesTest.java
@@ -1,38 +1,27 @@
 package gradesystem.services;
 
 import gradesystem.Allocator;
-import gradesystem.config.CourseInfo;
 import gradesystem.config.TA;
 import java.util.Arrays;
-import java.util.Collection;
 import org.junit.Test;
 import utils.UserUtilities;
 
 import gradesystem.Allocator.SingletonAllocation;
+import gradesystem.config.ConfigurationInfo;
+import java.util.List;
 
 import static org.junit.Assert.*;
-import org.easymock.EasyMock;
-
-//import static org.easymock.EasyMock.*;
-//Normally would be imported like this (as is done for junit),
-//but to make it clearer for this demo what EasyMock is being
-//used for, it has not been.
+import static org.easymock.EasyMock.*;
 
 public class UserServicesTest
 {
-    /**
-     * See more on EasyMock here:
-     * http://easymock.org/EasyMock3_0_Documentation.html
-     */
-
-
     @Test
     public void isUserTA_test()
     {
         //Mocked User Utilities
-        final UserUtilities userUtils = EasyMock.createMock(UserUtilities.class);
-        EasyMock.expect(userUtils.getUserLogin()).andReturn("jak2");
-        EasyMock.replay(userUtils);
+        final UserUtilities userUtils = createMock(UserUtilities.class);
+        expect(userUtils.getUserLogin()).andReturn("jak2");
+        replay(userUtils);
 
         SingletonAllocation<UserUtilities> userUtilsAlloc =
             new SingletonAllocation<UserUtilities>()
@@ -40,26 +29,26 @@ public class UserServicesTest
                 public UserUtilities allocate() { return userUtils; };
             };
 
-        //Mocked Course Info
-        final CourseInfo courseInfo = EasyMock.createMock(CourseInfo.class);
+        //Mocked Configuration Info
+        final ConfigurationInfo configurationInfo = createMock(ConfigurationInfo.class);
 
-        TA ta = EasyMock.createMock(TA.class);
-        EasyMock.expect(ta.getLogin()).andReturn("jak2");
-        EasyMock.replay(ta);
-        Collection<TA> tas = Arrays.asList(new TA[] { ta });
+        TA ta = createMock(TA.class);
+        expect(ta.getLogin()).andReturn("jak2");
+        replay(ta);
+        List<TA> tas = Arrays.asList(new TA[] { ta });
 
-        EasyMock.expect(courseInfo.getTAs()).andReturn(tas);
-        EasyMock.replay(courseInfo);
+        expect(configurationInfo.getTAs()).andReturn(tas);
+        replay(configurationInfo);
 
-        SingletonAllocation<CourseInfo> courseInfoAlloc =
-            new SingletonAllocation<CourseInfo>()
+        SingletonAllocation<ConfigurationInfo> configurationInfoAlloc =
+            new SingletonAllocation<ConfigurationInfo>()
             {
-                public CourseInfo allocate() { return courseInfo; };
+                public ConfigurationInfo allocate() { return configurationInfo; };
             };
 
         //Customizer the Allocator with mocked objects
         new Allocator.Customizer()
-                .setCourseInfo(courseInfoAlloc)
+                .setConfigurationInfo(configurationInfoAlloc)
                 .setUserUtils(userUtilsAlloc)
                 .customize();
 
@@ -67,8 +56,8 @@ public class UserServicesTest
         assertEquals(true, new UserServicesImpl().isUserTA());
 
         //Verify that mocked methods were called
-        EasyMock.verify(userUtils);
-        EasyMock.verify(ta);
-        EasyMock.verify(courseInfo);
+        verify(userUtils);
+        verify(ta);
+        verify(configurationInfoAlloc);
     }
 }
