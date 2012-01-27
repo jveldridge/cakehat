2b5cac69a208e13bae650ab5789b4bcd4057d774
hannah.d.rosen <hannah.d.rosen@414fe6b6-edb3-11de-91ba-91c70a792394>
2012-01-27 06:01:31 +0000

    Resolves issue none : Adds DataServices : Adds DataServicesV5 and DataServicesV5Impl files. Also adds getDataServicesV5() method to Allocator. : None : http://cakehat.cs.brown.edu/foodcritic/r/436/diff/#index_header

---
+++ src/cakehat/newdatabase/DataServicesV5Impl.java
@@ -0,0 +1,16 @@
+package cakehat.newdatabase;
+
+import cakehat.assignment.Part;
+import cakehat.services.ServicesException;
+
+/**
+ *
+ * @author Hannah
+ */
+public class DataServicesV5Impl implements DataServicesV5 {
+
+    public TA getGrader(Part part, Group group) throws ServicesException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
