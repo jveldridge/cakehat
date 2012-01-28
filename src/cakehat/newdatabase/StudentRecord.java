a60415fc463ea2de5b0af82b6dacfd97b9912bc9
yudifu.d@gmail.com <yudifu.d@gmail.com@414fe6b6-edb3-11de-91ba-91c70a792394>
2012-01-28 05:12:13 +0000

    Resolves issue none : Adds database method signatures not needed by config manager : Adds the rest of database method signatures. : none : http://cakehat.cs.brown.edu/foodcritic/r/448/

---
+++ src/cakehat/newdatabase/StudentRecord.java
@@ -0,0 +1,57 @@
+package cakehat.newdatabase;
+
+/**
+ * Represents information about a student as stored in the database.  Unlike the
+ * {@link Student} object, instances of this class are not managed and are not 
+ * guaranteed to be valid.  This class should used only as a return type for
+ * {@link Database} methods supporting {@link DataServices} methods.
+ * 
+ * @author jeldridg
+ */
+class StudentRecord {
+    
+    private final int _dbId;
+    private final String _login, _firstName, _lastName, _email;
+    private final boolean _isEnabled, _hasCollab;
+
+    StudentRecord(int dbId, String login, String firstName, String lastName, 
+                    String email, boolean isEnabled, boolean hasCollab) {
+        _dbId = dbId;
+        _login = login;
+        _firstName = firstName;
+        _lastName = lastName;
+        _email = email;
+        _isEnabled = isEnabled;
+        _hasCollab = hasCollab;
+    }
+    
+    public int getDbId() {
+        return _dbId;
+    }
+
+    public String getLogin() {
+        return _login;
+    }
+
+    public String getFirstName() {
+        return _firstName;
+    }
+
+    public String getLastName() {
+        return _lastName;
+    }
+    
+    public String getEmail() {
+        return _email;
+    }
+
+    public boolean isEnabled() {
+        return _isEnabled;
+    }
+    
+    public boolean hasCollab() {
+        return _hasCollab;
+    }
+    
+}
+
